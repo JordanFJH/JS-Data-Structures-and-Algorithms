c0607b049b6a21d1b59c3e6c17c78ac0042f5fee
Jordan <higgsfj@gmail.com>
2024-02-20 18:13:48 -0500

    drop constant example

--- Coding Files/BigO.js
+++ Coding Files/BigO.js
@@ -3,6 +3,11 @@ function logItems(n) {
     for (let i = 0; i < n; i++) {
         console.log(i)
     }
+
+    // Drop constant example
+    for (let j = 0; j < n; j++) {
+        console.log(i)
+    }
 }
 
-logItems(10)
+// logItems(10)
