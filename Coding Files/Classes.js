bad7b3e63ddbc7d50ba9fc1e8c4f6fab59dab1e9
Jordan <higgsfj@gmail.com>
2024-02-21 13:27:22 -0500

    made class file

---
+++ Coding Files/Classes.js
@@ -0,0 +1,18 @@
+// Class Example
+class Cookie {
+    constructor(color) {
+        this.color = color
+    }
+    // Methods
+    getColor() {
+        return this.color
+    }
+    setColor(color) {
+        this.color = color
+    }
+}
+
+let cookieOne = new Cookie("green")
+
+let cookieTwo = new Cookie("blue")
+
